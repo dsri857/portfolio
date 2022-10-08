d882faf78efa6142e0bdca2b7fa659afd531ddc8
dsri857 <113185686+dsri857@users.noreply.github.com>
2022-10-08 19:51:38 +0530

    Add files via upload

---
+++ tgt.js
@@ -0,0 +1,71 @@
+// gsap.registerPlugin(ScrollTrigger);
+// // REVEAL //
+// gsap.utils.toArray(".revealUp").forEach(function (elem) {
+//   ScrollTrigger.create({
+//     trigger: elem,
+//     start: "top 80%",
+//     end: "bottom 20%",
+//     markers: true,
+//     onEnter: function () {
+//       gsap.fromTo(
+//         elem,
+//         { y: 100, autoAlpha: 0 },
+//         {
+//           duration: 1.25,
+//           y: 0,
+//           autoAlpha: 1,
+//           ease: "back",
+//           overwrite: "auto"
+//         }
+//       );
+//     },
+//     onLeave: function () {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+//     },
+//     onEnterBack: function () {
+//       gsap.fromTo(
+//         elem,
+//         { y: -100, autoAlpha: 0 },
+//         {
+//           duration: 1.25,
+//           y: 0,
+//           autoAlpha: 1,
+//           ease: "back",
+//           overwrite: "auto"
+//         }
+//       );
+//     },
+//     onLeaveBack: function () {
+//       gsap.fromTo(elem, { autoAlpha: 1 }, { autoAlpha: 0, overwrite: "auto" });
+//     }
+//   });
+// });
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
